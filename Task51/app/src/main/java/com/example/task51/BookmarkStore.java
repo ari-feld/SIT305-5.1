61da36a764003092be9e059b57c27dd742e891ef
Ari <s220238775@deakin.edu.au>
2026-04-14 20:21:34 +1000

    14/4

---
+++ Task51/app/src/main/java/com/example/task51/BookmarkStore.java
@@ -0,0 +1,11 @@
+package com.example.task51;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BookmarkStore {
+    public static List<NewsItem> bookmarks = new ArrayList<>();
+
+
+
+}
